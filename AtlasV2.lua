216c3b8bd179b14994ff601b7dec883571e241ef
xz <95953670+laagginq@users.noreply.github.com>
2024-02-24 01:50:03 -0800

    Update AtlasV2.lua

--- AtlasV2.lua
+++ AtlasV2.lua
@@ -186,7 +186,7 @@ function library.new(library_title, cfg_location)
         Name = "Main",
         AnchorPoint = Vector2.new(0.5, 0.5),
         BackgroundColor3 = Color3.fromRGB(15, 15, 15),
-        BorderColor3 = Color3.fromRGB(78, 93, 234),
+        BorderColor3 = Color3.fromRGB(111, 50, 168),
         Position = UDim2.new(0.5, 0, 0.5, 0),
         Size = UDim2.new(0, 700, 0, 500),
         Image = "http://www.roblox.com/asset/?id=7300333488",
